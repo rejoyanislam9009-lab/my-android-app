82688531d82dca2754069e501821e75fe63a2c02
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-08-21 15:03:21 +0300

    chore: bump Guide version to 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guide.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.2.0"
+        versionCode = 5
+        versionName = "2.3.0"
     }
 
     buildTypes {
