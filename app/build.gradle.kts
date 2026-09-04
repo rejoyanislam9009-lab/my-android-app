de23b9fb466b3e9f32bc0f248a1be44bd9b14916
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-09-04 05:04:03 +0300

    Target Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.rejoy.bdvpn"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.rejoy.bdvpn"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
