c12499201bf5edfa520a73bade2479866c435dfc
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-09-05 22:38:52 +0300

    release: bump GlobalCall to v1.7.0 contact identity upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.globalcall.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.6.1"
+        versionCode = 24
+        versionName = "1.7.0"
 
         buildConfigField("String", "FIREBASE_API_KEY", "\"AIzaSyAGr0BN1CbqiL_WcgZ8br20Np14zp_8NaE\"")
         buildConfigField("String", "FIREBASE_PROJECT_ID", "\"guide-c4c2c\"")
